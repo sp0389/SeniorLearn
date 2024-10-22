1703b8038ac0f530952e4c9027ba2785c595907f
Graeme Bevins <graeme.bevins@studytafensw.edu.au>
2024-10-22 21:25:02 +1100

    feat: set up initial groundwork including models, service, and MongoDB setup for bulletin feature

---
+++ SeniorLearn/Data/Bulletin.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Bson;
+
+namespace SeniorLearn.Data
+{
+    public enum BulletinStatus
+    {
+        Active,
+        Archived
+    }
+
+    public class Bulletin
+    {
+        public ObjectId Id { get; set; }
+        public string Title { get; set; } = default!;
+        public string MemberId { get; set; } = default!;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string ContentImageUrl { get; set; } = default!;
+        public string ContentMessage { get; set; } = default!;
+        public List<string> Tags { get; set; } = new();
+        public int Likes { get; set; }
+        public BulletinStatus Status { get; set; } = BulletinStatus.Active;
+        public List<BulletinComment> Comments { get; set; } = new();
+    }
+
+    public class BulletinComment
+    {
+        public ObjectId Id { get; set; }
+        public ObjectId BulletinId { get; set; } = default!;
+        public string MemberId { get; set; } = default!;
+        public string ProfileImageUrl { get; set; } = default!;
+        public DateTime CommentedAt { get; set; }
+        public string Message { get; set; } = default!;
+    }
+}
---
+++ SeniorLearn/Services/BulletinService.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using SeniorLearn.Data;
+
+namespace SeniorLearn.Services
+{
+    public class BulletinService
+    {
+        // TODO: Update later. Localhost for now.
+
+        private const string connectionString = "mongodb://localhost:27017";
+        private readonly IMongoCollection<Bulletin> _bulletinCollection;
+
+        public BulletinService()
+        {
+            var mongoClient = new MongoClient(connectionString);
+            _bulletinCollection = mongoClient.GetDatabase("bulletin_db")
+                .GetCollection<Bulletin>("bulletin");
+        }
+
+        //TODO: Service Methods for bulletins GET/POST/UPDATE etc.
+    }
+}
